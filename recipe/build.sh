81dadcb90418369a36fd02153d029562834cdf18
Maksim Rakitin <mrakitin@bnl.gov>
2020-01-16 17:52:29 -0500

    Almost working configuration

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
 set -e
 
 test -d m4 || mkdir m4
 gtkdocize || exit 1
 
-export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
-export ACLOCAL_PATH="$PREFIX/share/aclocal"
+export python=$PYTHON
+export PATH=$BUILD_PREFIX/bin:$PATH
+export PKG_CONFIG_LIBDIR="$BUILD_PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PKG_CONFIG_LIBDIR
+export ACLOCAL_PATH="$BUILD_PREFIX/share/aclocal"
 aclocal --print-ac-dir
 
-$PREFIX/bin/autoreconf -ivf
+autoreconf -ivf
+
 ./configure --disable-gui --enable-introspection=yes --disable-hkl-doc --prefix=$PREFIX 
 # || { cat config.log ; exit 1 ; }
-make
+make -j
 make install
