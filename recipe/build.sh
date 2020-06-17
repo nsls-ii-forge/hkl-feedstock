92ae44b1917b2071fc13c3155cdf4d2fed5044c9
Maksim Rakitin <mrakitin@bnl.gov>
2020-06-17 16:33:15 -0400

    Sync with https://github.com/conda-forge/staged-recipes/pull/10819

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,18 +3,13 @@
 set -e
 
 test -d m4 || mkdir m4
-gtkdocize || exit 1
+# gtkdocize || exit 1
 
-export python=$PYTHON
-export PATH=$BUILD_PREFIX/bin:$PATH
-export PKG_CONFIG_LIBDIR="$BUILD_PREFIX/lib/pkgconfig"
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PKG_CONFIG_LIBDIR
-export ACLOCAL_PATH="$BUILD_PREFIX/share/aclocal"
+export ACLOCAL_PATH="$PREFIX/share/aclocal"
 aclocal --print-ac-dir
 
 autoreconf -ivf
 
-./configure --disable-gui --enable-introspection=yes --disable-hkl-doc --prefix=$PREFIX 
-# || { cat config.log ; exit 1 ; }
-make -j
+./configure --disable-static --disable-gui --enable-introspection=yes --disable-hkl-doc --prefix=$PREFIX
+make -j ${CPU_COUNT}
 make install
